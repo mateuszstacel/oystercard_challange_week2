0dfec4f3d5e1be296985a726c7a4d7e653847054
mateuszstacel <mateusz.stacel92@gmail.com>
2018-11-05 18:39:49 +0000

    Updated card with deduct

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,20 @@ class Oystercard
 
 attr_reader :balance
 
+MAX_BALANCE = 90
+
   def initialize(balance = 0)
     @balance = balance
+
   end
 
   def top_up(amount)
+    fail 'Max limit is £90'  if (@balance + amount) > MAX_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,8 +12,24 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+     expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'raises an error if the maximum balance is over limit' do
+        maximum_balance = Oystercard::MAX_BALANCE
+        subject.top_up(maximum_balance)
+        expect{ subject.top_up 1 }.to raise_error 'Max limit is £90'
+      end
+
+  end
+
+  describe '#deduct' do
+    it {is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'deduct money from card'do
+      subject.top_up(20)
+      expect{ subject.deduct 3 }.to change {subject.balance }.by -3
     end
   end
 
-  end
+end
