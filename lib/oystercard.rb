8f06d969f1d2f28ba4c85d3fe18608f5639d4b13
mateuszstacel <mateusz.stacel92@gmail.com>
2018-11-05 16:33:44 +0000

    Added top_up method

---
+++ lib/oystercard.rb
@@ -0,0 +1,14 @@
+class Oystercard
+
+attr_reader :balance
+
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
+
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,19 @@
+require 'oystercard'
+
 describe Oystercard do
 
-  
-end
+  describe '#balance' do
+    it 'it is check if initialize balance is 0' do
+    expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#topup' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
+
+  end
