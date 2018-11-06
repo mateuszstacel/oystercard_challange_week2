908cc15eb27777511801cf40b10d3f8b4a6ada30
mateuszstacel <mateusz.stacel92@gmail.com>
2018-11-06 13:55:36 +0000

    Updated with not allow to entry if balance < 1

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,16 +6,33 @@ MAX_BALANCE = 90
 
   def initialize(balance = 0)
     @balance = balance
-
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail 'Max limit is £90'  if (@balance + amount) > MAX_BALANCE
+    fail 'Max limit is #{MAX_BALANCE}'  if (@balance + amount) > MAX_BALANCE
     @balance += amount
   end
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    allow_entry
+   @in_journey = true
+  end
+
+  def touch_out
+   @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def allow_entry
+    fail 'not enought money on card' if @balance < 1
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,6 @@ describe Oystercard do
 
   describe '#topup' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'can top up the balance' do
      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
@@ -18,18 +17,46 @@ describe Oystercard do
     it 'raises an error if the maximum balance is over limit' do
         maximum_balance = Oystercard::MAX_BALANCE
         subject.top_up(maximum_balance)
-        expect{ subject.top_up 1 }.to raise_error 'Max limit is £90'
+        expect{ subject.top_up 1 }.to raise_error 'Max limit is #{MAX_BALANCE}'
       end
 
   end
 
   describe '#deduct' do
     it {is_expected.to respond_to(:deduct).with(1).argument }
-
     it 'deduct money from card'do
       subject.top_up(20)
       expect{ subject.deduct 3 }.to change {subject.balance }.by -3
     end
   end
+
+  describe '#touch_in' do
+    it { is_expected.to respond_to :touch_in }
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to :touch_out }
+  end
+
+  describe '#in_journey?' do
+    it { is_expected.to respond_to :in_journey? }
+    it 'return true if card in use' do
+      allow(subject).to receive(:in_journey?).and_return(true)
+      expect(subject.in_journey?).to be true
+    end
+    it ' return false if card not in use' do
+      allow(subject).to receive(:in_journey?).and_return(true)
+      expect(subject.in_journey?).to be true
+    end
+  end
+
+    describe '#allow_entry' do
+      it {is_expected.to respond_to :allow_entry}
+      it 'raise error if balance < 1' do
+      balance = 0
+      expect{ subject.allow_entry }.to raise_error 'not enought money on card'
+    end
+
+    end
 
 end
